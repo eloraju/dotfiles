8f4100aaa3945bcf5d433ee61cfe1eaeef41d4b0
Juuso Elo-Rauta <juuso.er@gmail.com>
2023-01-12 23:39:26 +0200

    i should go to sleep.... i'm starting to do dump stuff

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #!/bin/sh
 
-# Install all packages
-sudo pacman -S --noconfirm git
+install_paru() {
+# Clone, install and cleanup
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si --noconfirm
+  cd .. && rm -rf paru
+}
+
+# Make sure git is installed sudo stuff still unclear
+#sudo pacman -S --noconfirm git
+#install_paru
 
 ## install paru to get aur stuff
 
 
 ## call paru to install everything we need
 
-# Copy all confs from this repo to their places
-#mv ./confs/* $HOME/.config
-#
-## Make vim install all plugins
+# Make symlinks of all confs
+for d in $(ls -d confs); do
+  echo $d
+done
+
+# Make vim install all plugins
 #nvim -es +PackerSync

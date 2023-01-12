c5b1f7fd6ff04acdf2d00d90612bd1bf7138776d
Juuso Elo-Rauta <juuso.er@gmail.com>
2023-01-12 22:50:02 +0200

    nvim configs to start off the new dotfiles repo

---
+++ nvim/lua/eloraju/set.lua
@@ -0,0 +1,33 @@
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+-- no auto backups
+vim.opt.swapfile = false
+vim.opt.backup = false
+
+-- long running undo history :)
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true;
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+-- alaways show at least 8 rows
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+-- maybe use a function to determine when to use this
+-- it's just a highlighted column at the 80 char mark
+-- very useful e.g. with markup
+vim.opt.colorcolumn = "80"
+
+vim.g.mapleader = " "

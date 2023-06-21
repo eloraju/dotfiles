3099dcebf7ec5c647f084262da36ca1232a42576
Juuso Elo-Rauta <juuso.er@gmail.com>
2023-06-21 11:09:33 +0300

    remove 'help' from treesitter conf

--- home/.config/nvim/after/plugin/treesitter.lua
+++ home/.config/nvim/after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the four listed parsers should always be installed)
-  ensure_installed = { "javascript", "typescript", "lua", "vim", "help", "rust", "svelte" },
+  ensure_installed = { "javascript", "typescript", "lua", "vim", "rust", "svelte" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,

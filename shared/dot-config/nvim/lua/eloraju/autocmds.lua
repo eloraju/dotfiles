fc317b769b35f7fa454d152fd1cdc4bbb768a483
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-06-30 11:39:08 +0300

    add super simple shebang insertion autcommand, could be expanded in the future

--- shared/dot-config/nvim/lua/eloraju/autocmds.lua
+++ shared/dot-config/nvim/lua/eloraju/autocmds.lua
@@ -8,3 +8,16 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+-- Create templates when opening a non-existing file
+vim.api.nvim_create_autocmd('BufNewFile', {
+  desc = 'Insert template into new script files',
+  group = vim.api.nvim_create_augroup('eloraju-insert-template-script', { clear = true }),
+  pattern = { '*.py', '*.sh', '*.ts', '*.zsh' },
+  callback = function(opts)
+    local template_path = vim.fn.stdpath 'config' .. '/lua/eloraju/templates/scripts'
+    local extension = opts.file:match '^.+%.(.+)$'
+    local template = template_path .. '/' .. extension
+    vim.cmd('silent 0r ' .. template)
+  end,
+})

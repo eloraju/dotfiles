002c1b124e7bbb099e19db748e67c2480731612e
Juuso Elo-Rauta <juuso.er@gmail.com>
2024-07-10 07:55:34 +0300

    some minor nvim changes

--- shared/dot-config/nvim/lua/eloraju/plugins/which-key.lua
+++ shared/dot-config/nvim/lua/eloraju/plugins/which-key.lua
@@ -6,7 +6,7 @@ return { -- Useful plugin to show you pending keybinds.
 
     -- Document existing key chains
     require('which-key').register {
-      ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+      ['<leader>k'] = { name = 'Code actions', _ = 'which_key_ignore' },
       ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },

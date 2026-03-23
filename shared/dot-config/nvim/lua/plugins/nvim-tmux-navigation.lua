9a0295e8de92b7cf0f515cef819e4a6d50c0a006
Juuso Elo-Rauta <juuso.er@gmail.com>
2026-03-23 09:30:35 +0200

    tmux navigation working

--- shared/dot-config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ shared/dot-config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -1,13 +1,26 @@
 return {
   "alexghergh/nvim-tmux-navigation",
-  config = function()
-    local tmux_nav = require("nvim-tmux-navigation")
-    vim.keymap.set("n", "<C-h>", tmux_nav.NvimTmuxNavigateLeft)
-    vim.keymap.set("n", "<C-j>", tmux_nav.NvimTmuxNavigateDown)
-    vim.keymap.set("n", "<C-k>", tmux_nav.NvimTmuxNavigateUp)
-    vim.keymap.set("n", "<C-l>", tmux_nav.NvimTmuxNavigateRight)
-    vim.keymap.set("n", "<C-\\>", tmux_nav.NvimTmuxNavigateLastActive)
-    vim.keymap.set("n", "<C-Space>", tmux_nav.NvimTmuxNavigateNext)
-  end,
-  enabled = true,
+  keys = {
+    {
+      "<C-h>",
+      "<Cmd>NvimTmuxNavigateLeft<cr>",
+      desc = "Navigate left",
+    },
+    {
+      "<C-j>",
+      "<Cmd>NvimTmuxNavigateDown<cr>",
+      desc = "Navigate down",
+    },
+    {
+      "<C-k>",
+      "<Cmd>NvimTmuxNavigateUp<cr>",
+      desc = "Navigate up",
+    },
+    {
+      "<C-l>",
+      "<Cmd>NvimTmuxNavigateRight<cr>",
+      desc = "Navigate right",
+    },
+  },
+  config = true,
 }
